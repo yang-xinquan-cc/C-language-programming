ddc29d06887008bb485a8eaca11abde4af181f72
杨新全 <143598720+yang-xinquan-cc@users.noreply.github.com>
2024-10-16 16:00:59 +0800

    更新 2-1最值查找.cpp

--- 第一章语言基础/竞赛常用库函数/2-1最值查找.cpp
+++ 第一章语言基础/竞赛常用库函数/2-1最值查找.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(){
 	int arr[10]={3,1,2,4,6,9,0,8,7};
 	//数组名其实也是指针 
-	//min_element max_element(初始地址，起始地址) 返回的都是迭代器，需要*取出对应的数 
+	//min_element max_element(初始地址，起始地址) 返回的都是地址，需要*取出对应的数 
 	cout<<*max_element(arr,arr+10)<<'\n';
 	cout<<*min_element(arr,arr+10)<<'\n';
 	return 0;
