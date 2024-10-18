2d475d1e99f4effc1e2ed722bdb2647cecf58fad
杨新全 <143598720+yang-xinquan-cc@users.noreply.github.com>
2024-10-18 14:13:18 +0800

    排序

--- 第一章语言基础/总结(习题)/排序-宝藏排序Ⅱ.cpp
+++ 第一章语言基础/总结(习题)/排序-宝藏排序Ⅱ.cpp
@@ -12,7 +12,7 @@ int main()
   ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
   int n;
   cin>>n;
-  for(int i=1;i<=n;++i){
+  for(int i=1;i<=n;++i){//对于数组的操作,建议从1开始到n,避免有些题挖的坑,对0下标的处理 
     cin>>a[i];
   }
   sort(a+1,a+1+n);
