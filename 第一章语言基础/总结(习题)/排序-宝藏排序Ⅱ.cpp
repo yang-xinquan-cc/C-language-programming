e18ae9b3568f28b7fcdfe2a5506ad2ffdee778ff
杨新全 <143598720+yang-xinquan-cc@users.noreply.github.com>
2024-10-18 14:11:48 +0800

    排序

---
+++ 第一章语言基础/总结(习题)/排序-宝藏排序Ⅱ.cpp
@@ -0,0 +1,24 @@
+//≈≈–Ú
+//±¶≤ÿ≈≈–Ú¢Ú
+//https://www.lanqiao.cn/problems/3226/learning/?page=1&first_category_id=1&problem_id=3226
+#include <bits/stdc++.h>
+using namespace std;
+
+int const N=1e5+50;
+int a[N];
+
+int main()
+{
+  ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+  int n;
+  cin>>n;
+  for(int i=1;i<=n;++i){
+    cin>>a[i];
+  }
+  sort(a+1,a+1+n);
+  for(int i=1;i<=n;++i){
+    cout<<a[i]<<" ";
+  }
+  cout<<'\n';
+  return 0;
+}
